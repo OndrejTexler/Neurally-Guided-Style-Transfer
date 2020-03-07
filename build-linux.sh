1aab54ed7aa685c54add9296d379ffe090decf5d
Aneta <moravcova92@gmail.com>
2020-03-07 21:36:05 +0100

    Add linux run script

---
+++ build-linux.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+g++ CreateGuideUtils.cpp \
+	EbsynthWrapper.cpp \
+	main.cpp \
+	OpenCVUtils.cpp \
+	patch_based_synthesis/src/ebsynth.cpp \
+	patch_based_synthesis/src/ebsynth_cpu.cpp \
+	patch_based_synthesis/src/ebsynth_nocuda.cpp \
+	-DNDEBUG \
+	-O6 \
+	-fopenmp \
+	-I"opencv-4.2.0/include" \
+	-I"patch_based_synthesis/include" \
+	-std=c++11 \
+	-o styletransfer \
+	-L"opencv-4.2.0/lib" \
+	-lopencv_world
